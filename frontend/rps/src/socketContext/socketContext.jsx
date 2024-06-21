a309f138d50c62d939350254ec8b2a3f3728ed5e
Sameeer-Ahmad <sam9910333@gmail.com>
2024-06-21 21:50:32 +0530

    makes some changes

--- frontend/rps/src/socketContext/socketContext.jsx
+++ frontend/rps/src/socketContext/socketContext.jsx
@@ -8,9 +8,8 @@ const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:4000');
+    const newSocket = io('https://rock-paper-scissors-ckq2.onrender.com/');
     setSocket(newSocket);
-
     return () => newSocket.close();
   }, []);
 
